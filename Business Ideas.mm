b4d34403ae62fd8d490bf7c38b5809cdf121c3a9
Other Realm <the@otherrealm.org>
2023-07-25 21:30:51 -0400

    Adding the vscode python setup files & other init

---
+++ Business Ideas.mm
@@ -0,0 +1,207 @@
+<map version="docear 1.1" dcr_id="1689346221901_cb42cueo6jse3r0of09l944mh" project="144CC68822F6UB67EX9CD56T527DDL7H8ZDN" project_last_home="file:/E:/Documents/Biz/mindmap/">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Example Business Ideas" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1690331062010">
+<hook NAME="AutomaticEdgeColor" COUNTER="7"/>
+<hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="Times New Roman" SIZE="12" BOLD="false" ITALIC="false"/>
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Default">
+<font SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="For profit ideas" POSITION="right" ID="ID_1937585169" CREATED="1689457844004" MODIFIED="1689457859391">
+<edge COLOR="#00ffff"/>
+<node TEXT="Flexible self reproducing robots" ID="ID_1232671594" CREATED="1689354386303" MODIFIED="1689457844022" MOVED="1689457844026">
+<node TEXT="Possible methods" ID="ID_1524247375" CREATED="1689445218144" MODIFIED="1689445245035">
+<node TEXT="A system that uses electrically generated magnets and iron ferrite partials suspended in a flexible media" ID="ID_1546857924" CREATED="1689442503446" MODIFIED="1690324694967" MOVED="1689445218157"/>
+<node TEXT="Localized thermal expansion/contraction" ID="ID_508143822" CREATED="1689445250427" MODIFIED="1689445368365"/>
+<node TEXT="Water pressure differences" ID="ID_810198225" CREATED="1689445375005" MODIFIED="1689445406346"/>
+</node>
+</node>
+<node TEXT="Moon Power" ID="ID_64711797" CREATED="1689354447761" MODIFIED="1689457910567" MOVED="1689457910571">
+<node TEXT="The idea is to use tidal changes in water depth (more than just waves) to lift large objects attached to a geared generator." ID="ID_1266502358" CREATED="1689452609877" MODIFIED="1689458652190"/>
+<node TEXT="Back of the envelop calculation: ((500**3*9807N*10m)/(6h*60min*60s))*4=2270138888.88889W*h/day" ID="ID_1689064838" CREATED="1689450932972" MODIFIED="1689453319795">
+<hook URI="file:/C:/Develop/SparkIgnite/img/tideMath2023-07-15%20155626.png" SIZE="0.6451613" NAME="ExternalObject"/>
+</node>
+<node TEXT="If we put it in the Bay of Fundy in Canada with 2 tides of ~16m, we could get up to: 3632.222MW*h/day" ID="ID_827351076" CREATED="1689452446360" MODIFIED="1690331044333"/>
+<node TEXT="OR: Use dielectric elastomer transducers" ID="ID_165166549" CREATED="1690330895323" MODIFIED="1690330944588"/>
+</node>
+<node TEXT="Custom shoes" ID="ID_1428917651" CREATED="1689459498740" MODIFIED="1689459513633">
+<node TEXT="According to the latest census data, 18% of American adults have trouble walking or going up steps for some reason or another[i]. .... Until recently, custom shoes were very expensive and not widely available because each one needed to be manually fabricated.  However, in the last decade or so, computer controlled 3D printing has made possible the prospect of automating custom designs and the forecast for 3D-printed footwear puts the revenue potential at around $5.9 billion by the end of the decade[iii]. ...See attachment for full text" ID="ID_1594933062" CREATED="1689459631812" MODIFIED="1689459861819"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      According to the latest census data, 18% of American adults have trouble walking or going up steps for some reason or another<a href="https://www.otherrealm.org/#_edn1" name="_ednref1" title="" id="_ednref1"><font face="Times New Roman,serif">[i]</font></a>.&#160;&#160;The inability to walk confidently without pain, discomfort, or fear of falling puts a toll, not just on the challenge of getting where you need to go, but also on having the desire to try and get there in the first place.&#160;&#160;Those who have gait abnormalities often have limited financial means to spend on custom orthotics, or do not have the wherewithal to seek it out unless a doctor or therapist suggests it.&#160;&#160;Wearing proper footwear, however, is an important preventative factor in predicting whether further injuries will occur and the amount of exercise and social engagement that will be carried out.&#160;&#160;The proper footwear therefore, can mean the difference between an individual retaining their job, leading a productive life, and contributing to society vs getting fired or going on disability insurance.&#160;&#160;Those who cannot find proper footwear often need expensive therapy and contribute to the trillions of dollars<a href="https://www.otherrealm.org/#_edn2" name="_ednref2" title="" id="_ednref2"><font face="Times New Roman,serif">[ii]</font></a>&#160;United States taxpayers currently have to spend insuring elderly and disabled adults.&#160;&#160;Low cost preventative measures are crucial to mitigating these drains on our collective pocketbook and psyche.&#160;
+    </p>
+    <p>
+      &#160;
+    </p>
+    <p>
+      Until recently, custom shoes were very expensive and not widely available because each one needed to be manually fabricated.&#160;&#160;However, in the last decade or so, computer controlled 3D printing has made possible the prospect of automating custom designs and the forecast for 3D-printed footwear puts the revenue potential at around $5.9 billion by the end of the decade<a href="https://www.otherrealm.org/#_edn3" name="_ednref3" title="" id="_ednref3"><font face="Times New Roman,serif"><sup>[iii]</sup></font></a>.&#160;&#160;While this technology is beginning to be rolled out for the high end sports market, it is still a nascent industry and people who diagnose and treat gait abnormalities often have limited skills in industrial design.&#160;&#160;Therefore, a need for simple tools that do not require an extensive background in many fields is a must.&#160;&#160;One aim of this study, therefore, is the automation of orthotic shoe design.&#160;&#160;In talking with existing orthotic fabricators, the current challenge in custom footwear is not directly in the technology behind 3D printing, but in the time it takes to design and print things.&#160;&#160;On the small scale, it is much more economical to create one-off shoes by hand, than it is to create them in CAD from scratch and wait to have them printed, and there is a shortage of CAD experts<a href="https://www.otherrealm.org/#_edn4" name="_ednref4" title="" id="_ednref4"><font face="Times New Roman,serif">[iv]</font></a>.&#160; However, with the advent of generative design practices, combined with open source gait analysis software using lower cost video tracking and software like <a href="https://simtk.org/projects/opensim">OpenSim</a>, the tools are available to automate the labor intensive parts of custom adaptive technology and thus lower costs.
+    </p>
+    <p>
+      
+    </p>
+    <div>
+      <div id="edn1">
+        <p class="MsoEndnoteText">
+          <a href="https://www.otherrealm.org/#_ednref1" name="_edn1" title="" id="_edn1"><font face="Times New Roman,serif" size="10.0pt">i]</font></a>&#8220;Percentage of any difficulty walking or climbing steps for adults aged 18 and over, United States, 2019&#8212;2020,&#8221; National Center for Health Statistics. Accessed: Feb. 19, 2022. [Online]. Available: <a href="https://wwwn.cdc.gov/NHISDataQueryTool/SHS_adult/index.html">https://wwwn.cdc.gov/NHISDataQueryTool/SHS_adult/index.html</a>
+        </p>
+        <p>
+          
+        </p>
+        <p>
+          
+        </p>
+      </div>
+      <div id="edn2">
+        <p class="MsoEndnoteText">
+          <a href="https://www.otherrealm.org/#_ednref2" name="_edn2" title="" id="_edn2"><font face="Times New Roman,serif" size="10.0pt">[ii]</font></a>&#160;&#8220;Annual Medicaid &amp; CHIP Expenditures | Medicaid.&#8221; <a href="https://www.medicaid.gov/state-overviews/scorecard/annual-medicaid-chip-expenditures/index.html">https://www.medicaid.gov/state-overviews/scorecard/annual-medicaid-chip-expenditures/index.html</a>&#160; (accessed Dec. 13, 2022).
+        </p>
+        <p>
+          
+        </p>
+        <p>
+          
+        </p>
+      </div>
+      <div id="edn3">
+        <p class="MsoEndnoteText">
+          <a href="https://www.otherrealm.org/#_ednref3" name="_edn3" title="" id="_edn3"><font face="Times New Roman,serif" size="10.0pt">[iii]</font></a>&#160;&#8220;3D-Printed Footwear 2019-2029, an Analysis of the Market Potential of 3D Printing in the Footwear Industry,&#8221; SmarTech Analysis, SMP-AM-FW-03, Mar. 2019. Accessed: Feb. 27, 2022. [Online]. Available: <a href="https://www.smartechanalysis.com/wp-content/uploads/2019/05/SmarTech_WP_Footwear.pdf">https://www.smartechanalysis.com/wp-content/uploads/2019/05/SmarTech_WP_Footwear.pdf</a>
+        </p>
+        <p>
+          
+        </p>
+        <p>
+          
+        </p>
+      </div>
+      <div id="edn4">
+        <p class="MsoEndnoteText">
+          <a href="https://www.otherrealm.org/#_ednref4" name="_edn4" title="" id="_edn4"><font face="Times New Roman,serif" size="10.0pt">[iv]</font></a>&#160;B. Mattear, &#8220;Shriner&#8217;s Children&#8217;s Hospital,&#8221; Mar. 28, 2022.
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Steps" ID="ID_124509185" CREATED="1689465471675" MODIFIED="1689465486750">
+<node TEXT="Make sure there is a market for the idea" ID="ID_952640526" CREATED="1689465519781" MODIFIED="1689467740175">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Create a business plan/research proposal
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Create a prototype to validate the theory that you can achieve corrective gait by changing the insole angle and the outsole width without overly restricting ankle movement" ID="ID_653427204" CREATED="1689467700196" MODIFIED="1689467959742">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Create a complete, auto customizable, application that takes gait data as input and a stl printable file as output" ID="ID_1820792478" CREATED="1689467964541" MODIFIED="1690331093541"/>
+<node ID="ID_869115968" CREATED="1690324947562" MODIFIED="1690325479764"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      More here... <a href="https://github.com/orgs/other-realm/projects/2/views/1">https://github.com/orgs/other-realm/projects/2/views/1</a>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Nonprofit/community" POSITION="right" ID="ID_1009148495" CREATED="1689457935220" MODIFIED="1689457961733">
+<edge COLOR="#ffff00"/>
+<node TEXT="Nonprofit dating app" ID="ID_1543857513" CREATED="1689354457160" MODIFIED="1689457935235" MOVED="1689457935239">
+<node TEXT="There are no effective dating apps or websites because the existing commercial ones only find other people who use the app - if we can make a non-profit dating organization that is made up of exclusively single people, then use big data to not only connect other active users but also connect other potential candidates who have no connection to the organization, we can attempt to overcome the lonesomeness that millions of people feel (by one study, over 60% of young people are lonely[1]).  Money can be rased by having a network of independent contactor matchmakers that revenue is shared with the organization.  To begin to overcome the inequalities that low income people feel, we could make it so that any matchmaker who works with a paying customer also has a consultation with the member who has been without a match for the longest timeframe." ID="ID_444739046" CREATED="1689454355970" MODIFIED="1689460098746"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      [1] &#8220;Loneliness in America: How the Pandemic Has Deepened an Epidemic of Loneliness,&#8221; Making Caring Common, Feb. 09, 2021. https://mcc.gse.harvard.edu/reports/loneliness-in-america (accessed Jul. 15, 2023).
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Scientific Research" POSITION="right" ID="ID_1813374269" CREATED="1689457987814" MODIFIED="1689458020005">
+<edge COLOR="#7c0000"/>
+<node TEXT="Quantum entangled Radar/Maser neural imaging as a possible means of telepathy" ID="ID_1382274689" CREATED="1689354573354" MODIFIED="1689457987835" MOVED="1689457987840"/>
+<node TEXT="Brain on a dish experiments with exogenous thought" ID="ID_926892785" CREATED="1689458039731" MODIFIED="1689458306155">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="14" FONT_FAMILY="Times New Roman" DESTINATION="ID_1382274689" STARTINCLINATION="176;0;" ENDINCLINATION="176;0;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+</node>
+</node>
+</node>
+</map>
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # From Spark to Ignition: Establishing a Plan for Entrepreneurial Success
-This is the repository where you can download everything we need for the UMass Amherst Engineering Class "From Spark to Ignition, Establishing a Plan for Entrepreneurial Success"
+This is the repository where you can download everything we need for the UMass Amherst Engineering Class "From Spark to Ignition, Establishing a Plan for Entrepreneurial Success"  
+It also has a bunch of things I have found useful in my educational journey.  
+
+List of things you will find here:  
+-  Instructions on how to set up a good developer environment  
+
